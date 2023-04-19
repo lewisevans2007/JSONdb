26b5d5334696f62bb7e45466fe7c8540939b4297
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-19 21:07:08 +0100

    Update comments
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
+/* JSONdb (Testing file)
+ * Create databases using JSON
+ * Github: https://github.com/awesomelewis2007/JSONdb
+ * By: Lewis Evans
+*/
+
 const JSONdb = require('../lib/index.js');
 
 const db = new JSONdb();
